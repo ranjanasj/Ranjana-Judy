d477c5f2659ac96688fbabf90b6b309c0790ff9f
Pfanner <pfannerj@global.anz.com>
2018-10-17 15:47:52 +1100

    Committing the changes

--- Temp.java
+++ Temp.java
@@ -8,15 +8,15 @@ public class Main {
         System.out.println("Please enter a temperature in celsius");
         int inTemp = scanner.nextInt();
         if (inTemp < 20) {
-            System.out.println("Too cold!!");
+            System.out.println("Too cold!! Need a jumper!");
         }
         else if (inTemp>20 ||inTemp <= 25)
  	{
-            System.out.println("Nice day!");
+            System.out.println("Nice day! But don't forget to Slip, Slop, Slap");
         }
 	else
 	{
-            System.out.println("Too Hot!");
+            System.out.println("Too Hot! Don't forget to stay hydrated!");
 	}		
     }
 }
